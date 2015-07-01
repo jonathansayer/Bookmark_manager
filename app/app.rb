4408e81e1c8953b2184faa1c5eb05e3ad92e8287
jonathansayer <jon.sayer1@gmail.com>
2015-07-01 15:51:45 +0100

    Updated app file

--- app/app.rb
+++ app/app.rb
@@ -7,50 +7,50 @@ require_relative './models/user'
 
 
 class AppWeb < Sinatra::Base
-  run! if app_file == $0
-  set :views, proc { File.join(root, '..', 'views') }
+ run! if app_file == $0
+ set :views, proc { File.join(root, '..', 'views') }
 
-  enable :sessions
-  set :session_secret, 'super secret'
+ enable :sessions
+ set :session_secret, 'super secret'
 
-  get '/links' do
-    @links = Link.all
-    erb :'links/index'
-  end
+ get '/links' do
+   @links = Link.all
+   erb :'links/index'
+ end
 
-  get '/links/new' do 
-     erb :new_links
-  end
+ get '/links/new' do 
+    erb :new_links
+ end
 
-  post '/links' do 
-    tags = params[:tag].split(" ")
-     link=Link.new(url: params[:url], 
-                   title: params[:title])
-    tags.each do |tag|
-      link.tags <<  Tag.create(name: tag)
-    end
-    link.save
-    redirect to ('/links')
-  end
+ post '/links' do 
+   tags = params[:tag].split(" ")
+    link=Link.new(url: params[:url], 
+                  title: params[:title])
+   tags.each do |tag|
+     link.tags <<  Tag.create(name: tag)
+   end
+   link.save
+   redirect to ('/links')
+ end
 
-  get '/tags/:name' do 
-    tag = Tag.first(name: params[:name])
-    @links = tag ? tag.links : []
-    erb :'links/index'
-  end
+ get '/tags/:name' do 
+   tag = Tag.first(name: params[:name])
+   @links = tag ? tag.links : []
+   erb :'links/index'
+ end
 
-  get '/users/new' do
-    erb:'users/new'
-  end
+ get '/users/new' do
+   erb:'users/new'
+ end
 
-  post '/users' do
-    current_user
-    redirect to('/links')
-  end
+ post '/users' do
+   current_user
+   redirect to('/links')
+ end
 
-  def current_user
-    User.create(email: params[:email],
-      password: params[:password])
-    session[:user_id] = user.id
-  end
+ def current_user
+   session[:user_id] = user.id
+   User.create(email: params[:email],
+     password: params[:password])
+ end
 end
